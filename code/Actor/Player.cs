61d3d4e510238ccd7ba28459314c95bb6e4de893
Titanovsky <titanovskyteam@gmail.com>
2025-06-16 00:22:45 +0500

    ебанная мразь

--- code/Actor/Player.cs
+++ code/Actor/Player.cs
@@ -1,4 +1,5 @@
 using Sandbox;
+using System;
 
 public sealed class Player : Component
 {
@@ -7,18 +8,40 @@ public sealed class Player : Component
     [Property] public float MaxHealth { get; set; } = 0f;
     [Property] public float Health { get; set; } = 0f;
     [Property] public int Dna { get; set; } = 0;
+    [Property] public CameraComponent Camera { get; set; }
 
-	private void Prepare()
+    public Action<Vector3> OnSpecified { get; set; }
+
+    private void Prepare()
 	{
 		MaxHealth = 100f;
 		Health = MaxHealth;
 		Dna = 0;
+
+        Camera = Scene.Camera;
 	}
 
     private void CreateSingleton()
 	{
 		if (Instance is null)
 			Instance = this;
+    }
+
+    public void Specify()
+    {
+        //var tr = Scene.Trace.Ray(Camera.WorldTransform.Forward, Camera.WorldTransform.Forward * 10000f).UseHitboxes(true).Size(new BBox(-5, 5)).Run();
+
+        //if (!tr.Hit) return;
+
+        //OnSpecified?.Invoke(tr.HitPosition);
+
+        //WorldPosition = tr.HitPosition;
+    }
+
+    private void CheckSpecify()
+    {
+        if (Input.Keyboard.Pressed("E"))
+            Specify();
     }
 
     protected override void OnStart()
@@ -30,5 +53,10 @@ public sealed class Player : Component
 	{
 		CreateSingleton();
 
+    }
+
+    protected override void OnUpdate()
+    {
+        CheckSpecify();
     }
 }
