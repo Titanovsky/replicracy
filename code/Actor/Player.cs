e029d25b253ce3cfa3339fa26eaef5a95a729b26
ChaseAndTront <139469156+ChaseAndTront@users.noreply.github.com>
2025-06-15 23:24:54 +0300

    Fixed ray trace

--- code/Actor/Player.cs
+++ code/Actor/Player.cs
@@ -3,7 +3,7 @@ using System;
 
 public sealed class Player : Component
 {
-	public static Player Instance { get; private set; }
+    public static Player Instance { get; private set; }
 
     [Property] public float MaxHealth { get; set; } = 0f;
     [Property] public float Health { get; set; } = 0f;
@@ -13,45 +13,48 @@ public sealed class Player : Component
     public Action<Vector3> OnSpecified { get; set; }
 
     private void Prepare()
-	{
-		MaxHealth = 100f;
-		Health = MaxHealth;
-		Dna = 0;
+    {
+        MaxHealth = 100f;
+        Health = MaxHealth;
+        Dna = 0;
 
         Camera = Scene.Camera;
-	}
+    }
 
     private void CreateSingleton()
-	{
-		if (Instance is null)
-			Instance = this;
+    {
+        if (Instance is null)
+            Instance = this;
     }
 
     public void Specify()
     {
-        //var tr = Scene.Trace.Ray(Camera.WorldTransform.Forward, Camera.WorldTransform.Forward * 10000f).UseHitboxes(true).Size(new BBox(-5, 5)).Run();
+        var tr = Scene.Trace.Ray(Camera.WorldPosition, Camera.WorldTransform.Forward * 10000f)
+            .UseHitboxes(true)
+            .Size(BBox.FromPositionAndSize(-5, 5))
+            .Run();
 
-        //if (!tr.Hit) return;
+        if (!tr.Hit) return;
 
-        //OnSpecified?.Invoke(tr.HitPosition);
+        OnSpecified?.Invoke(tr.HitPosition);
 
-        //WorldPosition = tr.HitPosition;
+        WorldPosition = tr.HitPosition;
     }
 
     private void CheckSpecify()
     {
-        if (Input.Keyboard.Pressed("E"))
+        if (Input.Pressed("Use"))
             Specify();
     }
 
     protected override void OnStart()
     {
-		Prepare();
+        Prepare();
     }
 
-	protected override void OnAwake()
-	{
-		CreateSingleton();
+    protected override void OnAwake()
+    {
+        CreateSingleton();
 
     }
 
