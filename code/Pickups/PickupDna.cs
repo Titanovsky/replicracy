28911c029ed0135ebcd2636d8de8fa982d73eece
Titanovsky <titanovskyteam@gmail.com>
2025-06-15 23:45:01 +0500

    add pickup_dna_small prefab and more size for hull add logic to add Dna and destroy yourself

--- code/Pickups/PickupDna.cs
+++ code/Pickups/PickupDna.cs
@@ -1,9 +1,20 @@
 using Sandbox;
 using System;
 
-public sealed class PickupDna : Component
+public sealed class PickupDna : Component, Component.ITriggerListener
 {
     [Property] public float Frequency { get; set; } = 5.43f;
     [Property] public float Amplitude { get; set; } = 6f;
     [Property] public float SpeedRotate { get; set; } = .25f;
+    [Property] public int Dna { get; set; } = 2;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var ply = other.Components.GetInAncestorsOrSelf<Player>();
+        if (!ply.IsValid()) return;
+
+        ply.Dna += Dna;
+
+        DestroyGameObject();
+    }
 }
