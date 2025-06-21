5636498b9b56d5b321481ff0b2ffe04ab8e44fb5
Titanovsky <titanovskyteam@gmail.com>
2025-06-21 14:19:55 +0500

    add basic villager behavior

--- code/Actor/NPC/Enemy/Villager.cs
+++ code/Actor/NPC/Enemy/Villager.cs
@@ -5,7 +5,10 @@ public sealed class Villager : EnemyBase
     public NavMeshAgent Agent { get; set; }
     private TimeUntil _delay;
 
-    protected override void OnStart()
+    private Vector3 _up = new Vector3(0, 0, 50f);
+    private Vector3 _targetPos;
+
+    private void Prepare()
     {
         GameObject.Name = $"😈 Enemy - {GameObject.Name}";
 
@@ -14,34 +17,45 @@ public sealed class Villager : EnemyBase
         _delay = 0f;
     }
 
-	protected override void OnUpdate()
-	{
+    private void Move()
+    {
+        var startPos = WorldPosition;
+
+        if (!_targetPos.IsNaN)
+        {
+            Gizmo.Draw.Color = Color.Orange;
+            Gizmo.Draw.LineThickness = 4;
+            Gizmo.Draw.Arrow(startPos + _up, _targetPos);
+        }
+
         if (!_delay) return;
 
-        var startPos = WorldPosition;
-        var point = Scene.NavMesh.GetClosestEdge(startPos, 8000f);
+        var point = Scene.NavMesh.GetRandomPoint(startPos, 256f);
 
-        //Agent.Velocity *= 50f;
-
-        Log.Info(point.Value);
+        //Agent.Velocity *= 2f;
 
         if (point.HasValue) // cuz GetClosesPoint give Vector3? (nullable) instead of Vector3
+        {
             Agent.MoveTo(point.Value);
 
-        Gizmo.Draw.Color = Color.Orange;
-        Gizmo.Draw.LineThickness = 5;
-        Gizmo.Draw.Arrow(startPos, point.Value);
+            _targetPos = point.Value;
+        }
 
-        _delay = 1f;
+        _delay = .75f;
     }
 
-    public override void OnTriggerEnter(Collider other)
+    protected override void OnStart()
     {
-        
+        Prepare();
+    }
+
+	protected override void OnUpdate()
+	{
+        Move();
     }
 
     public override void OnDamage(in DamageInfo damage)
     {
-
+        Log.Info($"Hit ");
     }
 }
