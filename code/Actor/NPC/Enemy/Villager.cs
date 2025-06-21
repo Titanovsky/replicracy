7b0bd51d2c1d8139a38d0f418ff91b55ce531f97
Titanovsky <titanovskyteam@gmail.com>
2025-06-21 09:15:24 +0500

    work navmesh agent (Villager)

--- code/Actor/NPC/Enemy/Villager.cs
+++ code/Actor/NPC/Enemy/Villager.cs
@@ -2,15 +2,38 @@
 
 public sealed class Villager : EnemyBase
 {
+    public NavMeshAgent Agent { get; set; }
+    private TimeUntil _delay;
+
     protected override void OnStart()
     {
         GameObject.Name = $"😈 Enemy - {GameObject.Name}";
+
+        Agent = GetComponent<NavMeshAgent>();
+
+        _delay = 0f;
     }
 
 	protected override void OnUpdate()
 	{
+        if (!_delay) return;
 
-	}
+        var startPos = WorldPosition;
+        var point = Scene.NavMesh.GetClosestEdge(startPos, 8000f);
+
+        //Agent.Velocity *= 50f;
+
+        Log.Info(point.Value);
+
+        if (point.HasValue) // cuz GetClosesPoint give Vector3? (nullable) instead of Vector3
+            Agent.MoveTo(point.Value);
+
+        Gizmo.Draw.Color = Color.Orange;
+        Gizmo.Draw.LineThickness = 5;
+        Gizmo.Draw.Arrow(startPos, point.Value);
+
+        _delay = 1f;
+    }
 
     public override void OnTriggerEnter(Collider other)
     {
