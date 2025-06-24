995da763d87f23fea4f09f113cb0d460bf3d4578
Titanovsky <titanovskyteam@gmail.com>
2025-06-24 23:35:10 +0500

    the first code for Cutscene

--- code/Cutscene/Cutscene.cs
+++ code/Cutscene/Cutscene.cs
@@ -6,9 +6,12 @@ public sealed class Cutscene : Component
     [Property, Feature("Info")] public string Description { get; set; } = "This is description";
 
     [Property, Feature("Stats")] public List<CutscenePoint> Points { get; set; } = new();
+    [Property, Feature("Stats")] public GameObject Obj { get; set; } //todo remove
     [Property, Feature("Stats")] public Action OnPlay { get; set; }
     [Property, Feature("Stats")] public Action OnFinish { get; set; }
     public bool IsPlaying { get; private set; } = false;
+    private int _currentPointIndex = -1;
+    private TimeUntil _delayMovingPoint;
 
     public void Play()
     {
@@ -26,6 +29,9 @@ public sealed class Cutscene : Component
         if (!IsPlaying) return;
 
         IsPlaying = false;
+        _currentPointIndex = -1;
+
+        //todo remove godmode player, new black screen and return camera to player
 
         OnFinish?.Invoke();
     }
@@ -40,8 +46,31 @@ public sealed class Cutscene : Component
     private void Move()
     {
         if (!IsPlaying) return;
+        if (Points.Count == 0) { Finish(); return; }
+        if (!_delayMovingPoint) return;
 
-        //
+        var point = GetPoint();
+
+        _delayMovingPoint = point.Delay;
+
+        Obj.WorldPosition = point.WorldPosition;
+        Obj.WorldRotation = point.WorldRotation;
+
+        if (point == GetLastPoint())
+            Finish();
+    }
+
+    private CutscenePoint GetPoint()
+    {
+        var index = (_currentPointIndex == -1) ? 0 : _currentPointIndex + 1;
+        if (index >= Points.Count) return Points[0];
+
+        return Points[index];
+    }
+
+    private CutscenePoint GetLastPoint()
+    {
+        return Points[Points.Count - 1];
     }
 
     protected override void OnFixedUpdate()
--- code/Cutscene/CutscenePoint.cs
+++ code/Cutscene/CutscenePoint.cs
@@ -1,4 +1,4 @@
 ﻿public sealed class CutscenePoint : Component
 {
-    [Property] public float Delay { get; set; } = 0f;
+    [Property] public float Delay { get; set; } = 5f;
 }
