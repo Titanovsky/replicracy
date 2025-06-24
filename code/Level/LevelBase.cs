f2d6a039361ea6b393a08cba930ca4cc20832628
Titanovsky <titanovskyteam@gmail.com>
2025-06-24 06:11:30 +0500

    LevelDowntown more properties

--- code/Level/LevelBase.cs
+++ code/Level/LevelBase.cs
@@ -1,8 +1,11 @@
 ﻿public abstract class LevelBase : Component
 {
-    [Property] public virtual string Class { get; set; } = "1";
-    [Property] public virtual string Name { get; set; } = "None";
-    [Property] public virtual SceneFile NextLevelScene { get; set; }
+    [Property, Group("Base")] public virtual string Class { get; set; } = "1";
+    [Property, Group("Base")] public virtual string Name { get; set; } = "None";
+    [Property, Group("Base")] public virtual SceneFile NextLevelScene { get; set; }
+
+    [Property, Group("Stats")] public virtual int NextDna { get; set; } = 0;
+    [Property, Group("Stats")] public virtual int NextFrags { get; set; } = 0;
 
     public virtual void Act(int act) { }
 
--- code/Level/LevelDowntown.cs
+++ code/Level/LevelDowntown.cs
@@ -1,8 +1,13 @@
-﻿public class LevelDowntown : LevelBase
+﻿public sealed class LevelDowntown : LevelBase
 {
-    [Property] public override string Class { get; set; } = "level1";
-    [Property] public override string Name { get; set; } = "Downtown";
-    [Property] public override SceneFile NextLevelScene { get; set; }
+    [Property, Group("Base")] public override string Class { get; set; } = "level1";
+    [Property, Group("Base")] public override string Name { get; set; } = "Downtown";
+    [Property, Group("Base")] public override SceneFile NextLevelScene { get; set; }
+
+    [Property, Group("Stats")] public override int NextDna { get; set; } = 15;
+    [Property, Group("Stats")] public override int NextFrags { get; set; } = 25;
+
+    [Property, Feature("Downtown")] public string Fuck { get; set; } = "das";
 
     public override void Act(int act)
     {
@@ -26,14 +31,22 @@
     }
 
     private void Act1()
-    { }
+    {
+        Log.Info($"The first act of start!");
+    }
 
     private void Act2()
-    { }
+    { 
+        //todo
+    }
 
     private void Act3()
-    { }
+    {
+        //todo
+    }
 
     private void Act4()
-    { }
+    {
+        //todo
+    }
 }
