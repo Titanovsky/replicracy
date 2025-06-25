bb4876414268bbaaf5942c1294d730438b2b248c
Titanovsky <titanovskyteam@gmail.com>
2025-06-25 17:03:43 +0500

    Gizmo for CutscenePoint

--- code/Cutscene/CutscenePoint.cs
+++ code/Cutscene/CutscenePoint.cs
@@ -1,4 +1,12 @@
 ﻿public sealed class CutscenePoint : Component
 {
     [Property] public float Delay { get; set; } = 5f;
+
+    protected override void DrawGizmos()
+    {
+        Gizmo.Transform = global::Transform.Zero;
+
+        Gizmo.Draw.Color = Color.Green;
+        Gizmo.Draw.LineSphere(WorldPosition, 2f);
+    }
 }
