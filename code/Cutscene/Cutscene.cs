87b4d3bd5229f1b6840d13eeec92296a69e4ebb9
ChaseAndTront <139469156+ChaseAndTront@users.noreply.github.com>
2025-06-25 18:56:17 +0300

    Updated Cutscene

--- code/Cutscene/Cutscene.cs
+++ code/Cutscene/Cutscene.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 public sealed class Cutscene : Component
 {
@@ -6,96 +7,162 @@ public sealed class Cutscene : Component
     [Property, Feature("Info")] public string Description { get; set; } = "This is description";
 
     [Property, Feature("Stats")] public List<CutscenePoint> Points { get; set; } = new();
-    [Property, Feature("Stats")] public GameObject Obj { get; set; } //todo remove after development
     [Property, Feature("Stats")] public Action OnPlay { get; set; }
     [Property, Feature("Stats")] public Action OnFinish { get; set; }
+
     public bool IsPlaying { get; private set; } = false;
     private int _currentPointIndex = -1;
     private TimeUntil _delayMovingPoint;
 
-    private Transform _objStartTransform; //todo remove after development 
+    private Player _player;
+    private CameraComponent _playerCamera;
+    private CutscenePoint _currentPoint;
 
-    //todo remove after development
+    private bool IsAttainCurrentPoint = false;
+
     private void StartTest()
     {
-        Play();
+        _ = Play();
     }
 
-    public void Play()
+    public async Task Play()
     {
         if (IsPlaying) return;
         if (Points.Count == 0) return;
 
         Log.Info($"[Cutscene] Start {Name}");
 
-        PreparePlay();
-
-        _objStartTransform = Obj.WorldTransform;
+        await PreparePlay();
+        await SetStartPosition();
 
         IsPlaying = true;
 
         OnPlay?.Invoke();
     }
 
-    public void Finish()
+    public async Task Finish()
     {
         if (!IsPlaying) return;
 
         IsPlaying = false;
         _currentPointIndex = -1;
 
-        //todo remove godmode player, new black screen and return camera to player
+        await InFadePlayer();
 
-        Obj.WorldTransform = _objStartTransform;
+        SetUsePlayerCameraControl(true);
+        _playerCamera = null;
+
+        await OutFadePlayer();
 
         Log.Info($"[Cutscene] Finish {Name}");
 
         OnFinish?.Invoke();
     }
 
-    private void PreparePlay()
+    private async Task PreparePlay()
     {
         //todo godmode player
-        //todo black screen
-        //todo prepare camera
+
+        await InFadePlayer();
+
+        _playerCamera = Scene.Camera;
+
+        SetUsePlayerCameraControl(false);
     }
 
-    private void Move()
+    private async Task SetStartPosition()
+    {
+        ChangeCurrentPoint();
+
+        _playerCamera.WorldPosition = _currentPoint.WorldPosition;
+        _playerCamera.WorldRotation = _currentPoint.WorldRotation;
+
+        await OutFadePlayer();
+    }
+
+    private void CheckPointPassed()
     {
         if (!IsPlaying) return;
+        if (!IsAttainCurrentPoint) return;
         if (!_delayMovingPoint) return;
 
-        if (_currentPointIndex == Points.Count - 1) { Finish(); return; }
-
-        var point = GetPoint();
-
-        _delayMovingPoint = point.Delay;
-
-        Obj.WorldPosition = point.WorldPosition;
-        Obj.WorldRotation = point.WorldRotation;
-
-        Log.Info($"[Cutscene] Moving {Name} {point.GameObject}");
+        ChangeCurrentPoint();
+        IsAttainCurrentPoint = false;
     }
 
-    private CutscenePoint GetPoint()
+    private void MoveToPoint()
+    {
+        if (!IsPlaying) return;
+        if (IsAttainCurrentPoint) return;
+        if (!_delayMovingPoint) return;
+
+        _playerCamera.WorldPosition = _playerCamera.WorldPosition
+            .LerpTo(_currentPoint.WorldPosition, _currentPoint.SpeedToPoint * Time.Delta, true);
+
+        var diff = _currentPoint.WorldPosition - _playerCamera.WorldPosition;
+
+        if (diff.LengthSquared < 0.01f)
+        {
+            IsAttainCurrentPoint = true;
+
+            _delayMovingPoint = _currentPoint.Delay;
+        }
+    }
+
+    private void RotateToPoint()
+    {
+        if (!IsPlaying) return;
+        if (IsAttainCurrentPoint) return;
+        if (!_delayMovingPoint) return;
+
+        var targetRotation = _currentPoint.WorldRotation;
+        var currentRotation = _playerCamera.WorldRotation;
+        var rotationDiff = Rotation.Difference(targetRotation, currentRotation);
+
+        _playerCamera.WorldRotation = currentRotation
+            .LerpTo(targetRotation, _currentPoint.SpeedToPoint * Time.Delta, true);
+    }
+
+    private void ChangeCurrentPoint()
     {
         var index = (_currentPointIndex == -1) ? 0 : _currentPointIndex + 1;
         _currentPointIndex = index;
 
-        if (index >= Points.Count) return Points[0];
+        if (index >= Points.Count)
+        {
+            _ = Finish();
+            return;
+        }
 
         Log.Info($"{index}");
 
-        return Points[index];
+        _currentPoint = Points[index];
     }
+
+    private void SetUsePlayerCameraControl(bool isControll) => _player.PlayerController.UseCameraControls = isControll;
 
     protected override void OnStart()
     {
+        _player = Player.Instance;
+
         StartTest();
+    }
+
+    protected override void OnDestroy()
+    {
+        _ = Finish();
+
+        _player = null;
     }
 
     protected override void OnFixedUpdate()
     {
-        Move();
+        CheckPointPassed();
+
+        MoveToPoint();
+        RotateToPoint();
     }
+
+    private async Task InFadePlayer() => await _player.Fade.InFade(1, 1);
+    private async Task OutFadePlayer() => await _player.Fade.OutFade(1);
 }
--- code/Cutscene/CutscenePoint.cs
+++ code/Cutscene/CutscenePoint.cs
@@ -1,6 +1,7 @@
 ﻿public sealed class CutscenePoint : Component
 {
     [Property] public float Delay { get; set; } = 5f;
+    [Property] public float SpeedToPoint { get; set; } = 3f;
 
     protected override void DrawGizmos()
     {
@@ -8,5 +9,8 @@
 
         Gizmo.Draw.Color = Color.Green;
         Gizmo.Draw.LineSphere(WorldPosition, 2f);
+
+        Gizmo.Draw.Color = Color.White;
+        Gizmo.Draw.Arrow(WorldPosition, WorldPosition + WorldRotation.Forward * 13f, 6f, 2.5f);
     }
 }
