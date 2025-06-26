967ec2f28f4efd1d24d55ea070a7d2dfce04d3dc
Titanovsky <titanovskyteam@gmail.com>
2025-06-26 06:52:48 +0500

    change to public ReplicantController.Replicants

--- code/Navigation/ReplicantController.cs
+++ code/Navigation/ReplicantController.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 public sealed class ReplicantController : Component
 {
     [Property] public Player Player { get; set; }
-    [Property] private List<Replicant> Replicants { get; set; } = new();
+    [Property] public List<Replicant> Replicants { get; set; } = new();
 
     private Vector3 _targetPoint;
     private GameObject _targeObject;
