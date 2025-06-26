df0d248661273810a9cabf1067d16f542ceaa154
Titanovsky <titanovskyteam@gmail.com>
2025-06-26 19:11:52 +0500

    add Replicracy.Common.Logger

---
+++ code/Other/Logger.cs
@@ -0,0 +1,26 @@
+﻿namespace Replicracy.Common;
+
+public class Logger
+{
+    public string Name { get; set; }
+
+    public Logger(string name)
+    {
+        Name = name;
+    }
+
+    public void Info(object msg)
+    {
+        Log.Info($"[{Name}] {msg}");
+    }
+
+    public void Warning(object msg)
+    {
+        Log.Warning($"[{Name}] {msg}");
+    }
+
+    public void Error(object msg)
+    {
+        Log.Error($"[{Name}] {msg}");
+    }
+}
