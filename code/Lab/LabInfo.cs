151c5386fbf2e80d6b2f8a3207ba5db8c2bccc55
Titanovsky <titanovskyteam@gmail.com>
2025-06-26 16:55:08 +0500

    properties for LabInfo (bodies and UnlockedAbilities)

--- code/Lab/LabInfo.cs
+++ code/Lab/LabInfo.cs
@@ -2,6 +2,29 @@
 {
     public static LabInfo Instance {  get; private set; }
 
+    [Property, Feature("Body")] public int BodyCost { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyCurrentHead { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyCurrentLeftHand { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyCurrentRightHand { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyCurrentLeftLeg { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyCurrentRightLeg { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyLabHead { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyLabLeftHand { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyLabRightHand { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyLabLeftLeg { get; set; } = 0;
+    [Property, Feature("Body"), Range(0, 3)] public int BodyLabRightLeg { get; set; } = 0;
+
+    [Property, Feature("Abilities")]
+    public List<bool> UnlockedAbitilies { get; set; } = new()
+    {
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+    };
+
     private void CreateSingleton()
     {
         if (Instance is null)
