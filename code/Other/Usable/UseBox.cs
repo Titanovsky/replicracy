78a48634933851f18f9a07428025abfafd502737
Titanovsky <titanovskyteam@gmail.com>
2025-06-26 06:47:30 +0500

    add delay for usebox

--- code/Other/Usable/UseBox.cs
+++ code/Other/Usable/UseBox.cs
@@ -5,10 +5,13 @@ public sealed class UseBox : Component, IUsable
 {
     [Property, TextArea] public string UsableText { get; set; }
     [Property] public bool IsUsed { get; set; } = false;
+    [Property] public float Delay { get; set; } = .75f;
     [Property] ModelRenderer boxModel { get; set; }
     [Property] HighlightOutline outline { get; set; }
 
     [Property] public Action OnCallback { get; set; }
+
+    private TimeUntil _delay;
 
     public void EnableHightlight()
     {
@@ -26,6 +29,9 @@ public sealed class UseBox : Component, IUsable
 
     public void Use()
     {
+        if (!_delay) return;
+        _delay = Delay;
+
         IsUsed = !IsUsed;
 
         ChangeColor();
